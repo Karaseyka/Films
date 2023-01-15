178560cc2797625aa6a568f4b46b5ddc62dc10d3
Karaseyka <vladchuprina12@mail.ru>
2023-01-15 21:57:38 +0300

    change main

--- app/src/main/java/com/example/films/Main.java
+++ app/src/main/java/com/example/films/Main.java
@@ -22,7 +22,7 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regestration);
+        setContentView(R.layout.activity_main);
 
 
 
