765adf9f7010730450700847ac2b067660ac69fa
Karaseyka <vladchuprina12@mail.ru>
2023-02-01 20:09:20 +0300

    add some files

---
+++ app/src/main/java/com/example/films/Film.java
@@ -0,0 +1,13 @@
+package com.example.films;
+
+public class Film {
+
+    private String name, url, txt;
+
+    public Film(String name, String url, String txt){
+        this.name = name;
+        this.url = url;
+        this.txt = txt;
+
+    }
+}
---
+++ app/src/main/java/com/example/films/Group.java
@@ -0,0 +1,4 @@
+package com.example.films;
+
+public class Group {
+}
